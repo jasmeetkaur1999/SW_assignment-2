33d90f9b4b3348b210c2400a435f5f82e1595c06
jasmeet kaur <jasmeet.bjf@gmail.com>
2020-01-08 12:06:50 -0600

    change main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 if('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-    .register('../service_worker.js')
+    .register('./service_worker.js')
     .then(reg => console.log('service worker: Registered')) 
     .catch(err=> console.log(`service worker: Error :${err}`))
   })
